211353d4d57757e16ed9ad55af441d37de7c0f10
Willian Bruno <mestrebruno@gmail.com>
2025-05-20 21:41:00 -0300

    Get Lists

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameListService;
+
+// frontend que chama o controlador(por essa api) e esse controlador que vai chamar o service, que vai chamar
+// repository que chama o banco de dados.
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+	
+	@Autowired
+	private GameListService gameListService;
+	
+	@GetMapping
+	public List<GameListDTO> findAll() {
+		List<GameListDTO> result = gameListService.findAll();
+		return result;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+
+@Service
+public class GameListService {
+
+	// O Spring vai injetar automaticamente a depencia GameRepository.
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	// Obedecendo principio ACID Atomic, Consistente, Isolada e Durável
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		List<GameList> result = gameListRepository.findAll();
+		// map vai transformar todo objeto original que era game para meu objeto GameMinDTO
+		// depois voce converte de novo apra uma lista normal com tolist.
+		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
+		return dto;
+
+	}
+	
+	
+
+}
